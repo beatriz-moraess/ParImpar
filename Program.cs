c281d092de914a3ec6f095a6a06b61456551406e
Beatriz <beatriz.giovanna@gmail.com>
2025-05-18 21:38:40 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Digite um número para descobrir se ele é par ou ímpar: ");
+int numero = Convert.ToInt32(Console.ReadLine()!);
+
+    if (numero % 2 == 0)
+    {
+    Console.WriteLine("O número é par!");
+    }
+    else
+    {
+    Console.WriteLine("O número é ímpar");
+    }
